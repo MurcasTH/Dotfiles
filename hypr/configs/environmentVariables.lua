ee61f86b4bd8a0acfaa872c2108e2d2a58b32e4d
Murcas <murcasTH@gmail.com>
2026-09-05 16:53:19 +0200

    Made pywal template for KDE and applied it

--- hypr/configs/environmentVariables.lua
+++ hypr/configs/environmentVariables.lua
@@ -4,12 +4,19 @@
 
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
 
+-- Cursor
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
+
+-- XDG
 hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
 hl.env("XDG_SESSION_DESKTOP", "Hyprland")
 hl.env("XDG_SESSION_TYPE", "wayland")
+
+-- Qt Theme
 hl.env("QT_QPA_PLATFORMTHEME", "hyprqt6engine")
+
+-- Japanese characters
 hl.env("XMODIFIERS", "@im=fcitx")
 hl.env("QT_IM_MODULE", "fcitx")
 hl.env("GTK_IM_MODULE", "fcitx")
